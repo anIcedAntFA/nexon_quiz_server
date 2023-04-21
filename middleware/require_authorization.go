8feecd05a32744831e017924c3a2a1985d18382d
anIcedAntFA <anicedfaant@gmail.com>
2023-04-21 17:19:04 +0700

    fix is_deleted user

--- middleware/require_authorization.go
+++ middleware/require_authorization.go
@@ -52,7 +52,7 @@ func RequiredAuthorization(appCtx appctx.AppContext) gin.HandlerFunc {
 			panic(err)
 		}
 
-		if user.IsDeleted == 0 {
+		if user.IsDeleted == 1 {
 			panic(common.ErrorNoPermission(errors.New("user has been deleted or banned")))
 		}
 
